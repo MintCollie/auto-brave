f3a7820da9ae28133f060c7f67e0edf65cb9a588
MintCollie <dragoncool57@yahoo.com>
2020-02-02 22:12:44 -0600

    autopilot tap error, asking r/learnrust for help
    ''

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
             .spawn()    //spawns it
             .expect("brave failed to start"); // or tells it that it failed
 
-    autopilot::key::tap(&T,&[],1,1);
+    let key_t = autopilot::key::Flag::Alt;
+    autopilot::key::tap(&key_t,&[],1,1);
     //use autopilot::key::Flag::Alt;
     //autopilot::key::tap(autopilot::key::Flag::Alt,&[],u64,u64);
 
