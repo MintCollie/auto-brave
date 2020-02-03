3dd8eccc23c457054cffc9176d47ddbcb4b60bef
MintCollie <dragoncool57@yahoo.com>
2020-02-02 21:52:19 -0600

    added basic readme and working on autiopilot tap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # brave-pilot
+
+Brave-pilot uses the autopilot crate to automate browsing in attempts to generate braves BAT.
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,8 @@ fn main() {
             .spawn()    //spawns it
             .expect("brave failed to start"); // or tells it that it failed
 
-    autopilot::key::tap(key: &ALT,0);
+    autopilot::key::tap(&T,&[],1,1);
+    //use autopilot::key::Flag::Alt;
+    //autopilot::key::tap(autopilot::key::Flag::Alt,&[],u64,u64);
+
 }
