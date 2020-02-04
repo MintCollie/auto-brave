2f31df4c704fd9c9ba75238be3f92752431d84c0
MintCollie <dragoncool57@yahoo.com>
2020-02-04 16:47:34 -0600

    switching from autopilot to enigo for handling of key presses. added 10 milisecond sleep in attempt to delay keypress.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-extern crate autopilot;
+//extern crate autopilot;
 use std::process::Command;
+use enigo::*;
+use std::{thread, time};
 
 fn main() {
+    //declaring varables
+    let mut enigo = Enigo::new();
+    let ten_millis = time::Duration::from_millis(10);
+    let _now = time::Instant::now();
 
 
     // spawns new instance of brave browser
     Command::new("brave")
             .spawn()    //spawns it
             .expect("brave failed to start"); // or tells it that it failed
-    autopilot::key::tap(&autopilot::key::Character('x'),&[],1,0);
+    thread::sleep(ten_millis);
+    enigo.key_click(Key::Layout('t'));
 }
